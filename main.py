bc5b62539d4f6c666a823f5e7a85486c5034698b
Olan Healy <olanhealy12@gmail.com>
2023-10-22 13:39:20 +0100

    Create CLI: Loops if cant find file, breaks if file found. Worling

--- main.py
+++ main.py
@@ -14,16 +14,28 @@ def opening():
 
 def main():
     opening()
-    file_name = input("Please enter the name of the PowerPoint file you wish to SummarAI, followed by .pptx: ")
-    search_pptx(file_name)
+
+    while True:
+        print()
+        print("##############################")
+        print()
+        file_name = input("Please enter the name of the PowerPoint file you wish to SummarAI, followed by .pptx: ")
+        if search_pptx(file_name):
+            break
 
 def search_pptx(file_name):
     print("Searching for pptx file, please make yourself a cup of tea")
 
     if file_name in os.listdir(rootDir):
         print("File Found. Let me SUMMARAI")
+        return True
     else:
         print("ERROR: File cannot be found. Please try again so we can SUMMURAI")
+        print()
+        print("##############################")
+        print()
+        return False
+       
 
 if __name__ == "__main__":
     main()
