263b6bf67789319d299336b3f5b7b04950bb2f4a
Olan Healy <olanhealy12@gmail.com>
2023-10-24 19:23:44 +0100

    Need pull to master for Kevin

--- main.py
+++ main.py
@@ -1,57 +1,30 @@
 import os
-import tkinter as tk
-from tkinterdnd2 import DND_FILES, TkinterDnD
+import customtkinter
+from tkinter import filedialog
 
-#script_dir = os.path.dirname(os.path.abspath(__file__))
-#rootDir = os.path.join(os.path.dirname(script_dir), "SummurAI", "pptx_files")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
 
-# def opening():
-    #print("  █████  █   █  ███████   ███████    █   █   ██████   ██████   ██████")
-    #print("  █      █   █  █  █  █   █  █   █   █   █   █    █   █    █     ██  ")
-    #print("  █      █   █  █  █  █   █  █   █   █   █   █    █   █    █     ██  ")
-    #print("  █████  █   █  █  █  █   █  █   █   █   █   █    █   ██████     ██  ")
-    #print("      █  █   █  █  █  █   █  █   █   █   █   █        █    █     ██  ")
-    #print("      █  █   █  █  █  █   █  █   █   █   █   █        █    █     ██  ")
-    # print("  █████  █████  █  █  █   █  █   █   █████   █        █    █   ██████")
-    
-
-
-   
-
-def on_file_drop(event):
-    file_path = event.data[1:-1]  
-    print(f"File path: {file_path}")
-    if file_path.lower().endswith('.pptx'):
-        message_label.config(text="SUCCESS", fg="green")
-        title.config(state='normal')
-        title.delete('1.0', 'end')
-        title.insert('end', f"Powerpoint Title: {file_path}")
-        title.config(state='disabled')
+def select_pptx_file():
+    file_path = filedialog.askopenfilename(
+        title="Select a .pptx file",
+        filetypes=[("PowerPoint files", "*.pptx")]
+    )
+    if file_path:
+        label.configure(text='SUCCESS: Selected PowerPoint file', font=("Helvetica", 24))
     else:
-        message_label.config(text="FAIL", fg="red")
+        label.configure(text='No file selected', font=("Helvetica", 24))
 
-root = TkinterDnD.Tk()
-root.title("SummarAI PowerPoint")
-root.geometry('1200x600')
+root = customtkinter.CTk()
+root.geometry("1200x800")
 
-title_label = tk.Label(root, text="SummurAI")
-title_label.pack()
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=0, padx=0, fill="both", expand=True)
 
-title = tk.Text(root, height=1, width=140)
-title.config(state='disabled', bg='#dddddd')
-title.pack()
+label = customtkinter.CTkLabel(master=frame, text="Please select a .pptx file", font=("Helvetica", 24))
+label.pack(pady=200, padx=0)
 
-message_label = tk.Label(root, text="Please drop a .pptx file here")
-message_label.pack()
-
-
-bottom_frame = tk.Frame(root, height=100, bg='#cccccc')
-bottom_frame.pack(fill='x', side='bottom')
-
-
-bottom_frame.drop_target_register(DND_FILES)
-bottom_frame.dnd_bind('<<Drop>>', on_file_drop)
+button = customtkinter.CTkButton(master=frame, text="Select PowerPoint File", command=select_pptx_file)
+button.pack(pady=20, padx=0)
 
 root.mainloop()
-
-
