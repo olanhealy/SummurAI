2d9d50c81a510bc2c8e4ecbb22e5bded48ad3e31
Kevin <kevinmscollins@gmail.com>
2023-11-25 17:08:00 +0000

    main.py added formatting from read.py

--- python/main.py
+++ python/main.py
@@ -1,18 +1,50 @@
 import customtkinter
 from tkinter import filedialog
 from pptx import Presentation
+from collections import Counter
 
 def set_appearance_and_theme():
     customtkinter.set_appearance_mode("dark")
     customtkinter.set_default_color_theme("green")
+    
+def get_font_size(paragraph):
+    """
+    Get the font size of a paragraph in points.
+    """
+    font_size = None
+    if paragraph.runs and paragraph.runs[0].font.size:
+        font_size = paragraph.runs[0].font.size.pt
+    return font_size
+
+def most_common_font_size(presentation):
+    """
+    Find the most common font size in a PowerPoint presentation.
+    """
+    font_sizes = []
+
+    for slide in presentation.slides:
+        for shape in slide.shapes:
+            if shape.has_text_frame:
+                for paragraph in shape.text_frame.paragraphs:
+                    font_size = get_font_size(paragraph)
+                    if font_size is not None:
+                        font_sizes.append(font_size)
+
+    if font_sizes:
+        most_common_size = Counter(font_sizes).most_common(1)[0][0]
+        return most_common_size
+    else:
+        return None
 
 # Edit of read.py here in new method
 def process_powerpoint_file(file_path, button_summurai):
     formatted_lines = []
-    default_font_size = 12
-
+    output_text = ""
     prs = Presentation(file_path)
 
+    # Calculate the most common font size
+    common_font_size = most_common_font_size(prs)
+    
     for slide in prs.slides:
         for shape in slide.shapes:
             if not shape.has_text_frame:
@@ -20,39 +52,44 @@ def process_powerpoint_file(file_path, button_summurai):
             for paragraph in shape.text_frame.paragraphs:
                 for run in paragraph.runs:
                     text = run.text
+                    font_size = get_font_size(paragraph)
                     line_info = {
                         "text": text,
-                        "is_heading": False,
+                        "is_key_text": False,
                         "is_bold": run.font.bold,
                         "is_italic": run.font.italic,
                         "is_underline": run.font.underline,
                     }
 
-                    font_size = run.font.size.pt if run.font.size is not None else default_font_size
-                    if font_size > 18:
-                        line_info["is_heading"] = True
+                    # Check if the line is Key Text based on font size
+                    if font_size is not None and font_size > 1.1 * common_font_size:
+                        line_info["is_key_text"] = True
+
+                    # Check if the line is a title based on its position in the slide
+                    if shape == slide.shapes[0]:
+                        line_info["is_key_text"] = True
+
                     formatted_lines.append(line_info)
 
-    output_text = ""
     for line_info in formatted_lines:
         text = line_info["text"]
-        is_heading = line_info["is_heading"]
+        is_key_text = line_info["is_key_text"]
         is_bold = line_info["is_bold"]
         is_italic = line_info["is_italic"]
         is_underline = line_info["is_underline"]
 
         output_text += "\n"
-        if is_heading:
-            output_text += f"H: {text}\n"
+        if is_key_text:
+            output_text += f"Key Text: {text}\n"
         else:
-            output_text += f"T: {text}\n"
+            output_text += f"Text: {text}\n"
 
         if is_bold:
-            output_text += "  - B\n"
+            output_text += "  - Bold\n"
         if is_italic:
-            output_text += "  - I\n"
+            output_text += "  - Italic\n"
         if is_underline:
-            output_text += "  - U\n"
+            output_text += "  - Underline\n"
 
     textbox.delete("1.0", "end")
     textbox.insert("1.0", output_text)
